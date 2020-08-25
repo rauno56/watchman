9fe0a41e039515aeab1b5e40e78d990b59950b9c
Rauno Viskus <Rauno56@gmail.com>
2020-08-25 11:25:06 +0300

    feat: add run command

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,16 @@ enum SubCommand {
         #[structopt(long = "name")]
         name: Option<String>,
     },
+    #[structopt(name = "run")]
+    /// Runs a command handling restarts and logs
+    ///
+    /// Example:
+    /// watchman run sleep 3
+    Run {
+        command: Vec<String>,
+        #[structopt(long = "log")]
+        output: Option<String>,
+    },
     #[structopt(name = "config")]
     /// Shows configuration file location
     Config,
@@ -119,27 +129,41 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
     let file_input: PathBuf = args.state_path.unwrap_or(get_state_path()?);
     let state_path = fs::canonicalize(file_input)?;
-    let mut state: State = State::from_file(&state_path)?;
 
     match args.cmd {
         Some(subcommand) => match subcommand {
-            SubCommand::Add { command, name } => state.add(command, name, None)?,
+            SubCommand::Run { command, output } => {
+                let s = dbg!(system::join(command));
+
+                let output = output.and_then(|val| Some(PathBuf::from(val)));
+                let output: Option<&PathBuf> = output.as_ref();
+                system::keep_running_from_string(&s, &String::from("kala"), output)?;
+            }
+            SubCommand::Add { command, name } => {
+                let mut state: State = State::from_file(&state_path)?;
+                state.add(command, name, None)?;
+                state.to_file(&state_path)?;
+            }
             SubCommand::Config => println!("{}", state_path.to_str().unwrap()),
             SubCommand::Fix => {
+                let mut state: State = State::from_file(&state_path)?;
                 state.fix_all()?;
                 show(&state);
+                state.to_file(&state_path)?;
             }
             SubCommand::Show => {
+                let mut state: State = State::from_file(&state_path)?;
                 state.update_all();
                 show(&state);
+                state.to_file(&state_path)?;
             }
         },
         None => {
+            let mut state: State = State::from_file(&state_path)?;
             state = interactive(state)?;
+            state.to_file(&state_path)?;
         }
     }
-
-    state.to_file(&state_path)?;
 
     Ok(())
 }
